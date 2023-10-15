f6129a109f24db9ef87ac2b35311b3be09eadaf6
Julio Ramirez <113139401+rzvdev1@users.noreply.github.com>
2023-10-14 21:25:48 -0700

    :construction: LocalStorage added

--- src/Context/Settings/SettingsComp.jsx
+++ src/Context/Settings/SettingsComp.jsx
@@ -5,16 +5,18 @@ export default function SettingsComp() {
   const { list, setList, hideCompleted } = useContext(SettingsContext);
 
   useEffect(() => {
-    setList([
-      {
-        id: 1,
-        text: 'Clean the Kitchen',
-        difficulty: 3,
-        assignee: 'Person A',
-        complete: hideCompleted,
-      },
-    ]);
-  }, [setList]);
+    // use local storage to save the list of todos and the hideCompleted setting
+    // use the useEffect hook to save the list and hideCompleted setting to local storage whenever either of these values changes
+
+    const list = localStorage.getItem('list');
+    const hideCompleted = localStorage.getItem('hideCompleted');
+    if (list) {
+      setList(JSON.parse(list));
+    }
+    if (hideCompleted) {
+      setList(JSON.parse(hideCompleted));
+    }
+  }, [list, hideCompleted]);
 
   return (
     <>
